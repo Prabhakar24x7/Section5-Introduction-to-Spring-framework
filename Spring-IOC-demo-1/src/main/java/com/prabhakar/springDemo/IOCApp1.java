d64addbbb3679742c612f406c294d2cf72aa8e2a
Prabhakar Singh <er.prabhakarsingh09@gmail.com>
2018-03-13 12:27:29 +0530

    Project initial commit

---
+++ Spring-IOC-demo-1/src/main/java/com/prabhakar/springDemo/IOCApp1.java
@@ -0,0 +1,25 @@
+package com.prabhakar.springDemo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class IOCApp1 {
+	
+	public static void main(String[] args) {
+		// create application context container
+		ApplicationContext ctx = new FileSystemXmlApplicationContext("beans.xml");
+		
+		// create bean
+		Organization org = (Organization)ctx.getBean("myorg");
+		
+		//invoking the company slogan via the bean
+		org.corporateSlogan();
+		
+		//close the application context ((container))
+		((FileSystemXmlApplicationContext)ctx).close();
+		
+	}
+	
+	
+	
+}
