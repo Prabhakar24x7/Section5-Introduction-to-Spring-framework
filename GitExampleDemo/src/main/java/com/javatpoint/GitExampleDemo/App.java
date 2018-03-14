78a406029204027a7493ab58f04b08e91f8a94ef
Prabhakar Singh <er.prabhakarsingh09@gmail.com>
2018-03-14 11:33:36 +0530

    Initial Commit by prabhakar

---
+++ GitExampleDemo/src/main/java/com/javatpoint/GitExampleDemo/App.java
@@ -0,0 +1,16 @@
+package com.javatpoint.GitExampleDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        Prabhakar p1 = new Prabhakar();
+        p1.method1();
+    }
+}
---
+++ GitExampleDemo/src/main/java/com/javatpoint/GitExampleDemo/Prabhakar.java
@@ -0,0 +1,8 @@
+package com.javatpoint.GitExampleDemo;
+
+public class Prabhakar {
+	
+	 public void method1(){
+		 System.out.println("Method by Prabhakar is called....");
+	 }
+}
