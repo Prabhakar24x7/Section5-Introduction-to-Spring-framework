ab68409f506b8ef3e24d52e1dac60d1feb990dc0
Prabhakar Singh <er.prabhakarsingh09@gmail.com>
2018-03-15 10:36:52 +0530

    Initial commit by prabhakar

---
+++ Spring-setter-demo/src/main/java/com/prabhakar/springDemo/Organization.java
@@ -0,0 +1,43 @@
+package com.prabhakar.springDemo;
+
+public class Organization {
+	
+	private String companyName;
+	private int yearOfIncorporation;
+	private String postalCode;
+	private int employeeCount;
+	
+	
+	
+	
+	public Organization(){}
+	
+	public Organization(String companyName, int yearOfIncorporation) {
+		this.companyName = companyName;
+		this.yearOfIncorporation = yearOfIncorporation;
+	}
+	
+	
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public void setEmployeeCount(int employeeCount) {
+		this.employeeCount = employeeCount;
+	}
+
+	public void corporateSlogan(){
+		
+		String slogan = "We build the altimate driving machine";
+		System.out.println(slogan);
+		
+	}
+
+	@Override
+	public String toString() {
+		return "Organization [companyName=" + companyName + ", yearOfIncorporation=" + yearOfIncorporation
+				+ ", postalCode=" + postalCode + ", employeeCount=" + employeeCount + "]";
+	}
+
+}
