99e34352a8a8a6cad63e527c18dd561fbe909c27
Prabhakar Singh <er.prabhakarsingh09@gmail.com>
2018-03-15 11:12:37 +0530

    Initial Commit

---
+++ Spring-constructor-arg-demo/src/main/java/com/prabhakar/springDemo/ConstructorArgsApp.java
@@ -0,0 +1,29 @@
+package com.prabhakar.springDemo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class ConstructorArgsApp {
+	
+	public static void main(String[] args) {
+		// create application context container
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans-cp.xml");
+		
+		// create bean
+		Organization org = (Organization)ctx.getBean("myorg");
+		
+		//invoking the company slogan via the bean
+		org.corporateSlogan();
+		
+		//Print organization details
+		System.out.println(org);
+		
+		//close the application context ((container))
+		((ClassPathXmlApplicationContext)ctx).close();
+		
+	}
+	
+	
+	
+}
